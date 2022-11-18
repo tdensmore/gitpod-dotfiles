3925e2a0227af7aaddb37c14f366d5643b6b836a
Todd Densmore <469399+tdensmore@users.noreply.github.com>
2022-11-18 12:56:33 -0500

    exporting GitHub email

--- scripts/github.sh
+++ scripts/github.sh
@@ -9,4 +9,5 @@ echo -e "$THIS_SCRIPT"
 THIS_TASK="$YELLOW->$DEFAULT"
 
 echo -e "$THIS_TASK setting user.email"
-git config user.email "469399+tdensmore@users.noreply.github.com"
+export GITPOD_GIT_USER_EMAIL="469399+tdensmore@users.noreply.github.com"
+# git config user.email "469399+tdensmore@users.noreply.github.com"
