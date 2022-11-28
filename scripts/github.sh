95f0ea129a322446b306f38e1dfe6576fd997eaa
todd densmore <469399+tdensmore@users.noreply.github.com>
2022-11-28 12:34:30 -0500

    Updated output message

--- scripts/github.sh
+++ scripts/github.sh
@@ -8,6 +8,7 @@ echo -e "$THIS_SCRIPT"
 
 THIS_TASK="$YELLOW->$DEFAULT"
 
-echo -e "$THIS_TASK setting user.email"
+echo -e "$THIS_TASK setting GITPOD_GIT_USER_EMAIL"
 export GITPOD_GIT_USER_EMAIL="469399+tdensmore@users.noreply.github.com"
+echo -e "$THIS_TASK setting user.email"
 # git config user.email "469399+tdensmore@users.noreply.github.com"
