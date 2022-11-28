ec82b42f9b9697bcf7f2d38ec6157cce649ec998
todd densmore <469399+tdensmore@users.noreply.github.com>
2022-11-28 13:06:59 -0500

    Exporting to bashrc

--- scripts/github.sh
+++ scripts/github.sh
@@ -9,6 +9,6 @@ echo -e "$THIS_SCRIPT"
 THIS_TASK="$YELLOW->$DEFAULT"
 
 echo -e "$THIS_TASK setting GITPOD_GIT_USER_EMAIL"
-export GITPOD_GIT_USER_EMAIL="469399+tdensmore@users.noreply.github.com"
+echo 'export GITPOD_GIT_USER_EMAIL="469399+tdensmore@users.noreply.github.com"' >> $HOME/.bashrc
 # echo -e "$THIS_TASK setting user.email"
 # git config user.email "469399+tdensmore@users.noreply.github.com"
